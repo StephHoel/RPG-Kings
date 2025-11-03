f1a7afb0dc6a62fe2c2115e7140a10427b1fb2dd
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-03 12:39:04 -0300

    feat(ui): add shared layout and button components + interfaces

--- src/components/Buttons.tsx
+++
@@ -1,7 +0,0 @@
-import { ButtonsWithClickProps } from '@/interfaces/ButtonsWithClickProps'
-
-export function Buttons({ onClick, disabled = false, children }: ButtonsWithClickProps) {
-  return (
-    <button className={`border rounded-lg px-4 py-2 w-full md:w-auto ${disabled && 'pointer-events-none'} `} onClick={onClick}>{children}</button>
-  )
-}
--- src/interfaces/ButtonsWithClickProps.ts
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export interface ButtonsWithClickProps extends PropsWithChildren {
-  onClick: () => void
-  disabled?: boolean
-}
