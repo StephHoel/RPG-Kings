49ba90cafa4c67518f0d6feb537f397d2d1557d8
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-09 21:39:23 -0300

    feat: improve route comparison by ensuring slashes are removed from both pathname and route

--- src/libs/others/compareRoutes.ts
+++ src/libs/others/compareRoutes.ts
@@ -1,9 +1,11 @@
 export function isSameRoute(pathname: string, route: string) {
   const path = pathname
     .replace(process.env.NEXT_PUBLIC_BASE_PATH ?? '', '')
+    .replaceAll('/','')
     .split('?')[0]
-
-  const routeClear = route
+    
+    const routeClear = route
+    .replaceAll('/','')
     .split('?')[0]
   
   return path === routeClear
