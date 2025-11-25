b01a18bdfc84c2cb94e8c2236acfe9ea3acc0d40
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-25 00:00:49 -0300

    refactor: update skills and stats seeds with race attributes and remove unused helper files

--- src/infra/dexie/seed/skills.seed.ts
+++ src/infra/dexie/seed/skills.seed.ts
@@ -1,68 +1,67 @@
-import { SKILL_ENUM, SKILL_TYPE } from '@/domain/constants'
+import { RACE_ENUM, SKILL_ENUM, SKILL_TYPE } from '@/domain/constants'
 import { Skill } from '@/infra/schemas'
 
 export const skillsSeed: Skill[] = [
   {
-    // TODO FIX incluir raças
     name: SKILL_ENUM.divination,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.banshee],
   },
   {
     name: SKILL_ENUM.electrokinesis,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.wiccan],
   },
   {
     name: SKILL_ENUM.elemental_manipulation,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.arcane, RACE_ENUM.kitsune],
   },
   {
     name: SKILL_ENUM.healing_others,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.arcane, RACE_ENUM.vampire],
   },
   {
     name: SKILL_ENUM.invisibility,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.wiccan],
   },
   {
     name: SKILL_ENUM.scream_control,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.banshee],
   },
   {
     name: SKILL_ENUM.sense_of_death,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.banshee],
   },
   {
     name: SKILL_ENUM.shapeshifting,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.kitsune, RACE_ENUM.shapeshift],
   },
   {
     name: SKILL_ENUM.sorcery,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.arcane],
   },
   {
     name: SKILL_ENUM.telekinesis,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.wiccan],
   },
   {
     name: SKILL_ENUM.teleportation,
     type: SKILL_TYPE.develop,
-    races: [],
+    races: [RACE_ENUM.wiccan],
   },
 
   {
     name: SKILL_ENUM.enhanced_sensations,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [RACE_ENUM.kanime, RACE_ENUM.werewolf, RACE_ENUM.shapeshift, RACE_ENUM.vampire],
   },
   {
     name: SKILL_ENUM.healing_saliva,
@@ -72,26 +71,39 @@ export const skillsSeed: Skill[] = [
   {
     name: SKILL_ENUM.longevity,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [
+      RACE_ENUM.arcane,
+      RACE_ENUM.kanime,
+      RACE_ENUM.kitsune,
+      RACE_ENUM.werewolf,
+      RACE_ENUM.shapeshift,
+      RACE_ENUM.vampire,
+    ],
   },
   {
     name: SKILL_ENUM.pain_absorption,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [RACE_ENUM.werewolf],
   },
   {
     name: SKILL_ENUM.paralyzing_venom,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [RACE_ENUM.kanime],
   },
   {
     name: SKILL_ENUM.premonition,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [RACE_ENUM.banshee],
   },
   {
     name: SKILL_ENUM.regeneration,
     type: SKILL_TYPE.fixed,
-    races: [],
+    races: [
+      RACE_ENUM.kanime,
+      RACE_ENUM.kitsune,
+      RACE_ENUM.werewolf,
+      RACE_ENUM.shapeshift,
+      RACE_ENUM.vampire,
+    ],
   },
 ]
