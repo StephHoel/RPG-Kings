d255afda4ccc2e5a84f975ecb2fd3cb4d75a9e67
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-27 20:41:26 -0300

    feat: apply suggestions from code review

--- src/domain/types/standard/genericTable.type.ts
+++ src/domain/types/standard/genericTable.type.ts
@@ -3,6 +3,5 @@ import { HeaderProps } from './header.type'
 export interface GenericTableProps<T> {
   header: HeaderProps<T>[]
   rows: T[]
-
   rowKey?: (row: T) => string | number
 }
--- src/infra/dexie/seed/animals.seed.ts
+++ src/infra/dexie/seed/animals.seed.ts
@@ -4,51 +4,51 @@ import { Animal } from '@/infra/schemas'
 export const animalsSeed: Animal[] = [
   {
     name: ANIMAL_ENUM.wolf,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.bear,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.lion,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.tiger,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.eagle,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.hawk,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.owl,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.snake,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.horse,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.fox,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.dog,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.cat,
-    race: RACE_ENUM.werewolf,
+    race: RACE_ENUM.shapeshift,
   },
   {
     name: ANIMAL_ENUM.kitsune,
